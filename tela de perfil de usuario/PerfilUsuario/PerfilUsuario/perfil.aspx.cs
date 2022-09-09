688b3bd03fc8b0f4b72de7d42e3fd2b02d48a830
Ada Vitoria <adavitoriapereiraferreira@gmail.com>
2022-09-08 22:14:14 -0300

    tela perfil 1.0

---
+++ tela de perfil de usuario/PerfilUsuario/PerfilUsuario/perfil.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PerfilUsuario
+{
+    public partial class perfil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAlterarCadastro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAtualizarCadastro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAlterarSenha_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAtualizarSenha_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
